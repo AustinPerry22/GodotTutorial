5d995c012654b29007c0b525dd085a720cff808d
Austin Perry <austinjperry22@gmail.com>
2023-11-21 13:17:47 -0700

    animations

--- scenes/Projectiles/grenade.gd
+++ scenes/Projectiles/grenade.gd
@@ -1,2 +1,5 @@
 extends RigidBody2D
 const speed = 750
+
+func explode():
+	$AnimationPlayer.play("Explosion")
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,9 +3,9 @@ extends Node2D
 var laser_scene: PackedScene = preload("res://scenes/Projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/Projectiles/grenade.tscn")
 
-func _on_gate_player_entered_gate(body):
-	print("player entered gate")
-	print(body)
+func _on_gate_player_entered_gate(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0,.5)
 
 
 func _on_player_laser(pos, direction):
@@ -21,3 +21,15 @@ func _on_player_grenade(pos, direction):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
+
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	tween.set_parallel(true)
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1)
+	#tween.tween_property($Player, "modulate:a",0,2).from(0.5)
+
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(.6,.6), 1)
